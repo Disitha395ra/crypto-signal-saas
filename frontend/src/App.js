992ad62a5aa482ee7d40cb895c94dfb5ef8c3083
Disitha395ra <disithar@gmail.com>
2026-02-05 14:52:00 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
